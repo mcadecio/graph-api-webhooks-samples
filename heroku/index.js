04741227a1894fba846c1b4d59e11d20e1b8ce74
Dercio Daio <33119595+mcadecio@users.noreply.github.com>
2024-03-21 15:08:41 +0000

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -21,42 +21,42 @@ var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
-  console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+    console.log(req);
+    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
-  if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
-  ) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.sendStatus(400);
-  }
+    if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == token
+    ) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
+    console.log('Facebook request body:', JSON.stringify(req.body));
 
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
+    if (!req.isXHubValid()) {
+        console.log('Warning - request header X-Hub-Signature not present or invalid');
+        res.sendStatus(401);
+        return;
+    }
 
-  console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
+    console.log('request header X-Hub-Signature validated');
+    // Process the Facebook updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
 });
 
 app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
+    console.log('Instagram request body:');
+    console.log(req.body);
+    // Process the Instagram updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
 });
 
 app.listen();
